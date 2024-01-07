d376c79b40931a2c573f05857acedfec66633981
Raymond Doetjes <rdoetjes@phonax.com>
2024-01-07 19:48:25 +0100

    added some sid music ;)

--- main.asm
+++ main.asm
@@ -1,6 +1,17 @@
+    .var music = LoadSid("Danger_zone.sid")
     BasicUpstart2(main)
     
 main:
+    lda #music.startSong-1
+    jsr music.init
+loop:
     inc $d020
     inc $d021
-    jmp main
+    lda $f0
+    cmp $d012
+    bne loop
+    jsr music.play
+    jmp loop
+
+ *=music.location "Music"
+.fill music.size, music.getData(i)
